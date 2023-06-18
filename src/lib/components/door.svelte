5110b97d53c0be826f210cdd0d082722f58c38a1
James Sartelle <james.sartelle@extra.app>
2023-06-18 14:24:52 -0500

    Basic layout

---
+++ src/lib/components/door.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { dev } from '$app/environment'
+
+	export let index = 0
+	export let isCar = false
+</script>
+
+<button class="door contrast outline" on:click>
+	<p>Door #{index + 1}</p>
+	{#if dev}
+		<p class="contents">{isCar ? '🚗' : '🐐'}</p>
+	{/if}
+</button>
+
+<style>
+	.door {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-align: center;
+		border: 1px solid currentColor;
+
+		&::after {
+			--size: 10px;
+			content: '';
+			border: 1px solid currentColor;
+			height: var(--size);
+			width: var(--size);
+			border-radius: 50%;
+			position: absolute;
+			top: calc(50% - (var(--size) / 2));
+			right: 10px;
+		}
+	}
+
+	.contents {
+		font-size: 2em;
+		border: 1px dashed currentColor;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Door from '$lib/components/door.svelte'
+
+	let doorCount = 3
+	let carIndex: number
+	$: carIndex = Math.floor(Math.random() * doorCount)
+</script>
+
+<main class="container">
+	<section class="controls">
+		<label for="doorCount">Door count: </label>
+		<input id="doorCount" bind:value={doorCount} type="number" min="0" />
+		<button on:click={() => doorCount--}>-</button>
+		<button on:click={() => doorCount++}>+</button>
+	</section>
+	<section class="doors">
+		{#each { length: doorCount } as _, i}
+			<Door index={i} isCar={i === carIndex} />
+		{/each}
+	</section>
+</main>
+
+<style lang="scss">
+	.controls {
+		display: flex;
+		align-items: center;
+		gap: var(--spacing);
+
+		label {
+			flex-shrink: 0;
+		}
+
+		:is(input, button) {
+			margin: 0;
+		}
+
+		button {
+			flex-basis: 25%;
+		}
+	}
+
+	.doors {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, 100px);
+		grid-auto-rows: 200px;
+		justify-content: space-evenly;
+	}
+</style>
